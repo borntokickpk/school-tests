eeaf23fdf3a297acad605024e28d3992e22d3aea
Muhammad Hassan Javed <93965089+hanzlajutt008@users.noreply.github.com>
2022-03-06 15:34:03 +0500

    Page not found added

--- main.js
+++ main.js
@@ -3,5 +3,7 @@ require("dotenv").config();
 
 const app = express();
 
+app.all("*", (_req, res) => res.status(404).json({ message: "Page not found" }));
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, console.log(`listening on port: ${PORT}`));
